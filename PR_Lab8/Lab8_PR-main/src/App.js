4004a8474f5b33a430e3fd0e6c82b20260d316e9
Narothe <kuba.dojka@gmail.com>
2023-05-12 12:42:23 +0200

    lab9

--- PR_Lab8/Lab8_PR-main/src/App.js
+++ PR_Lab8/Lab8_PR-main/src/App.js
@@ -1,14 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from "./components/navbar";
+import { Outlet } from "react-router-dom";
 
-import Posts from "./components/posts";
 
 function App() {
-  return (
-    <main className="container">
-      <Posts/>
-    </main>
-  );
+    return (
+        <div className="container-fluid">
+            <Navbar />
+            <div className="container">
+                <Outlet />
+            </div>
+        </div>
+    );
 }
 
 export default App;
