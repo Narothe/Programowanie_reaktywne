6fc34b051e92bf91f54fcab3abe758599631c8e2
Narothe <kuba.dojka@gmail.com>
2023-06-06 20:35:41 +0200

    works

--- PR_Lab11/PR_Lab11-master/src/components/common/navbar.jsx
+++ PR_Lab11/PR_Lab11-master/src/components/common/navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
                 <div style={{display: 'flex', justifyContent: 'space-between'}}>
                     <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
                         <li className="nav-item" role="presentation">
-                            <Link to="/" className="nav-link" id="pills-home-tab" data-toggle="pill" role="tab"
+                            <Link to="/home" className="nav-link" id="pills-home-tab" data-toggle="pill" role="tab"
                                   aria-controls="pills-home" aria-selected="true">Home</Link>
                         </li>
                         <li className="nav-item" role="presentation">
